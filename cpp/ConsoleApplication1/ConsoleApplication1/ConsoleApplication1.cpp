ffbf2013c532a29194fb02eb3f8d0519f6782abb
Ben Greco <benjamin.greco@codeworldwide.com>
2018-10-05 15:18:10 +0100

    Updated user

--- cpp/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ cpp/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
     string user;				// Define an empty string
-    user = "World";				// Populate the new string
+    user = "CST";				// Populate the new string
     cout << "Hello " + user + "!\n";		// Greet the user
 }
